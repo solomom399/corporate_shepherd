02b48ae442ed2c1eebd4c5ced82120e27f4ed8f5
solomom399 <sobanjosodiq@gmail.com>
2016-07-28 20:37:27 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -60,54 +60,11 @@ $("#ad-form").submit(function(){
    processData:false,
       cache: false,
       success: function(resp) {
-            if(resp == true){
-                    location.href = "../engine/view.php"
-            }else{
-                $(".check").text(resp).css("color","red")
-                $(".close").click()
-            }
-        }
-    })
-    return false
-})
-
-$(".vform").submit(function(){
-    $(".run").click()
-    $.ajax({
-      type: "POST",
-      url: "http://csga.com.ng/engine/verify.php",
-      data: new FormData(this),
-      contentType: false,
-         cache: false,
-   processData:false,
-      cache: false,
-      success: function(resp) {
-            if(resp == "Maximum limit raech"){
-              $(".su").html('<h3>'+resp+'</h3>').css("color","red")
-            }else{
-
-            location.href = "view.php"
-          }
-        }
-    })
-    return false
-})
-
-$(".get-btn").click(function(){
-    $(".run").click()
-    $.ajax({
-      type: "POST",
-      url: "http://csga.com.ng/engine/view.php",
-      success: function(resp) {
-            if(location.href = "view.php"){
-              $(".close").click()
-            }
             
         }
     })
     return false
 })
-
 
 
 $(function (){
